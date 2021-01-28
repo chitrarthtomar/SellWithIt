01381db4ab93a8a0b4bb4a36c37141008f1dfbb7
cheetah <chitrarth007@gmail.com>
2021-01-28 23:16:48 +0530

    added basic li

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Home from './app/screens/home/home';
 import Login from './app/screens/login/Login';
 
 export default function App() {
   let x = 1;
   console.log("app executed!")
   return (
-    <Login></Login>
+    // <Login></Login>
+    <Home></Home>
   );
 }
 
