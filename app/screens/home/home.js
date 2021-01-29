7b46355d051d1eb15444ec7f61db2c66922c5f55
cheetah <chitrarth007@gmail.com>
2021-01-29 23:58:09 +0530

    added search filtering for the list

--- app/screens/home/home.js
+++ app/screens/home/home.js
@@ -1,37 +1,44 @@
-import React from 'react'
-import { ScrollView, View, Text, Image, FlatList } from 'react-native'
+import React, { useState }  from 'react'
+import { ScrollView, View, Text, Image, FlatList, TextInput } from 'react-native'
 import { homeStyles } from './homeStyles';
 
-const lookup = [{ "id": 1, "name": "test1", "type": "paint", "costPrice": "12.34", "cartage": 4, "sellingPrice": 20 }, { "id": 23, "name": "nails", "type": "hardware", "costPrice": "12.34", "cartage": "4", "sellingPrice": "70" }, { "id": 3, "name": "fsfds", "type": "sanitory", "costPrice": "16.34", "cartage": "4", "sellingPrice": "50" }, { "id": 4, "name": "commode seat ", "type": "sanitory", "costPrice": "14.34", "cartage": "4", "sellingPrice": "40" }, { "id": 6, "name": "asian paint", "type": "paint", "costPrice": "230.34", "cartage": "21", "sellingPrice": "250" }, { "id": 7, "name": "nerolac", "type": "paint", "costPrice": "332", "cartage": "22", "sellingPrice": "500" }, { "id": 24, "name": "polymer", "type": "paint", "costPrice": "12.34", "cartage": "22", "sellingPrice": "15" }];
+const lookup = [{ "id": 1, "name": "Test1", "type": "paint", "costPrice": "12.34", "cartage": 4, "sellingPrice": 20 }, { "id": 23, "name": "nails", "type": "hardware", "costPrice": "12.34", "cartage": "4", "sellingPrice": "70" }, { "id": 3, "name": "fsfds", "type": "sanitory", "costPrice": "16.34", "cartage": "4", "sellingPrice": "50" }, { "id": 4, "name": "commode seat ", "type": "sanitory", "costPrice": "14.34", "cartage": "4", "sellingPrice": "40" }, { "id": 6, "name": "asian paint", "type": "paint", "costPrice": "230.34", "cartage": "21", "sellingPrice": "250" }, { "id": 7, "name": "nerolac", "type": "paint", "costPrice": "332", "cartage": "22", "sellingPrice": "500" }, { "id": 24, "name": "polymer", "type": "paint", "costPrice": "12.34", "cartage": "22", "sellingPrice": "15" }];
 
 const colDisplay = (labelText, value) => {
-    return(
+    return (
         <View style={homeStyles.priceInfo}>
-            <Text>{labelText}</Text>
-            <Text>{value}</Text>
+            <Text style={homeStyles.infoText}>{labelText}</Text>
+            <Text style={homeStyles.infoText}>{value}</Text>
         </View>
     );
 }
 
+const filterList = (data, search) => {
+    return data.filter(m => m.name.toLowerCase().indexOf(search) >= 0);
+}
 
 const Home = () => {
+    const [searchText, setSearchText] = useState("");
     return (
 
         <View style={homeStyles.container}>
-            <FlatList data={lookup} renderItem= {({item})=> 
-                    <View key={item.id} style={homeStyles.listItem}>
-                        <View style={homeStyles.topInfo}>
-                            <Image source={require("../../assets/saleCart.jpg")} style={homeStyles.logo} />
-                            <Text style={homeStyles.itemName}>{item.name}</Text>
-                        </View>
-                        <View style={homeStyles.bottomInfo}>
-                            {colDisplay("CostPrice", item.costPrice)}
-                            {colDisplay("Cartage", item.cartage)}
-                            {colDisplay("TotalCostPrice", +item.costPrice + +item.cartage)}
-                            {colDisplay("SellingPrice", item.sellingPrice)}
-                        </View>
+            <TextInput placeholder="Search here..." onChangeText={(search) =>  setSearchText(search.toLowerCase())} style={homeStyles.searchBar}></TextInput>
+            <ScrollView style={homeStyles.scrollView}>
+            <FlatList style={homeStyles.listView} data={filterList(lookup, searchText)} renderItem={({ item }) =>
+                <View key={item.id} style={homeStyles.listItem}>
+                    <View style={homeStyles.topInfo}>
+                        <Image source={require("../../assets/saleCart.jpg")} style={homeStyles.logo} />
+                        <Text style={homeStyles.itemName}>{item.name}</Text>
                     </View>
-                } />
+                    <View style={homeStyles.bottomInfo}>
+                        {colDisplay("CostPrice", item.costPrice)}
+                        {colDisplay("Cartage", item.cartage)}
+                        {colDisplay("TotalCostPrice", +item.costPrice + +item.cartage)}
+                        {colDisplay("SellingPrice", item.sellingPrice)}
+                    </View>
+                </View>
+            } />
+            </ScrollView>
         </View>
     );
 }
--- app/screens/home/homeStyles.js
+++ app/screens/home/homeStyles.js
@@ -5,35 +5,40 @@ export const homeStyles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "moccasin"
+        backgroundColor: "#EEF0F3",
     },
     listItem: {
         flex: 1,
         flexDirection: "column",
         padding: "10px",
-        borderColor: "cadetblue",
+        borderColor: "D8DDE1",
         borderWidth: "1px",
         marginVertical: "2px",
+        backgroundColor: "white",
+    },
+    listView: {
     },
     topInfo: {
         flexDirection: "row",
         alignItems: "center",
         borderBottomWidth: "1px",
         paddingBottom: "5px",
-        borderColor: "darkcyan",
+        borderColor: "D8DDE1",
     },
     bottomInfo: {
         marginTop: "5px",
         flexDirection: "row",
         alignItems: "center",
+        display: "flex",
+        justifyContent: "space-evenly",
     },
     priceInfo: {
         flexDirection: "column",
         alignItems: "center",
         paddingHorizontal: "10px",
-        borderRightWidth: "1px",
-        borderLeftWidth: "1px",
-        borderColor: "darkcyan",
+        // borderRightWidth: "1px",
+        // borderLeftWidth: "1px",
+        borderColor: "D8DDE1",
     },
     logo: {
         height: 30,
@@ -46,6 +51,23 @@ export const homeStyles = StyleSheet.create({
     itemName: {
         fontSize: "20px",
         fontFamily: "monospace",
-        fontWeight: "500"
+        fontWeight: "500",
+        color: "#585D61",
     },
+    searchBar: {
+        fontSize: 24,
+        margin: 10,
+        width: '80%',
+        height: 50,
+        borderRadius: "50px",
+        backgroundColor: 'white',
+        paddingLeft: "10px",
+      },
+      infoText: {
+        color: "#585D61",
+      },
+      scrollView: {
+          height: "90%",
+          width: "90%",
+      },
 });
